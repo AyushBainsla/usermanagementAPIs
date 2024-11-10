26a41d121512c8684d8390f272881f820f04ebf3
AyushBainsla <ayushbansla2@gmail.com>
2024-11-10 16:15:32 +0530

    vercel test

--- pages/api/auth.js
+++
@@ -1,6 +0,0 @@
-const app = require("../../app");  // Import your Express app
-
-// Export a handler for Vercel to use
-module.exports = (req, res) => {
-  app(req, res);  // Pass the request and response to your Express app
-};
